cfe12caa9d8b5546234ed8e1fd1e0c6a448326f6
Aaron Schlesinger <aschlesinger@deis.com>
2016-03-11 09:50:21 -0800

    porting code from gohttpbin
    https://github.com/arschles/gohttpbin
    
    Making slight modifications for running it as a standalone binary

---
+++ main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func get(resp http.ResponseWriter, req *http.Request) {
+	m := getBase(req)
+	output(resp, m)
+}
+
+func post(resp http.ResponseWriter, req *http.Request) {
+	m := getBase(req)
+	m[DataKey] = string(readBody(req))
+	output(resp, m)
+}
+
+func put(resp http.ResponseWriter, req *http.Request) {
+	m := getBase(req)
+	m[DataKey] = string(readBody(req))
+	output(resp, m)
+}
+
+func delete(resp http.ResponseWriter, req *http.Request) {
+	m := getBase(req)
+	output(resp, m)
+}
+
+func head(resp http.ResponseWriter, req *http.Request) {
+	m := getBase(req)
+	output(resp, m)
+}
+
+func patch(resp http.ResponseWriter, req *http.Request) {
+	m := getBase(req)
+	output(resp, m)
+}
+
+func main() {
+	port := flag.Int("port", 8080, "the port to listen on")
+
+	router := mux.NewRouter()
+	router.HandleFunc("/get", get).Methods("GET")
+	router.HandleFunc("/post", post).Methods("POST")
+	router.HandleFunc("/put", put).Methods("PUT")
+	router.HandleFunc("/delete", delete).Methods("DELETE")
+	router.HandleFunc("/head", head).Methods("HEAD")
+	router.HandleFunc("/patch", patch).Methods("PATCH")
+
+	hostStr := fmt.Sprintf(":%d", *port)
+	log.Printf("kubehttpbin listening on %s", hostStr)
+	http.ListenAndServe(hostStr, router)
+
+}
