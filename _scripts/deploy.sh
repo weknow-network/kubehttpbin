3414e303e79bf25359e695fde5255670cba586b3
Aaron Schlesinger <arschles@gmail.com>
2017-03-12 13:40:53 -0700

    Using helm to deploy (#6)
    * adding helm chart
    
    * removing old manifests
    
    * reorganizing values
    
    * adding a chart yaml
    
    * parameterizing the templates more
    
    * deploying with helm in the deploy script
    
    * adding the encrypted kubeconfig file
    
    as well as the decryption step
    
    * add the helm upgrade command
    
    along with all of the helm setup logic
    
    * removing master-only builds
    
    * removing the encrypted kubeconfig and using kubeconfig env var
    
    * removing decryption step
    
    the file and keys don’t exist anymore

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -5,6 +5,18 @@
 
 cd "$(dirname "$0")" || exit 1
 
+# push to quay
 export IMAGE_PREFIX=arschles
 docker login -e="$QUAY_EMAIL" -u="$QUAY_USERNAME" -p="$QUAY_PASSWORD" quay.io
 make -C .. docker-build docker-push
+
+# install the kubeconfig file from the env var, then tell helm where to find it 
+# (with the KUBECONFIG env var)
+echo $KUBECONFIG_DATA_BASE64 | base64 --decode > ./kubeconfig
+export KUBECONFIG=./kubeconfig
+
+# download the helm CLI and calling 'helm upgrade'
+curl -o helm https://storage.googleapis.com/kubernetes-helm/helm-v2.2.2-darwin-amd64.tar.gz
+chmod +x helm
+LAST_RELEASE=$(helm list -q)
+helm upgrade $LAST_RELEASE ../chart
